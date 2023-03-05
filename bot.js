f9839b99ea780b26b4403d047f83844b95298cd8
oliviacutee798 <125283931+oliviacutee798@users.noreply.github.com>
2023-03-05 18:18:51 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,68 +1,68 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf } = require('telegraf')
 const axios = require('axios');
 const moment = require('moment');
+const _ = require('lodash');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {
-  ctx.reply('Welcome to CryptoGecko Bot. Type /help for a list of commands.');
-});
-
-bot.help((ctx) => {
-  ctx.reply('/price <coin>: Get the price of a coin in USD\n/chart <coin>: Get the 7-day chart for a coin\n/news <coin>: Get the latest news for a coin\n/list: List all available coins');
+  ctx.reply('Welcome to your new bot!');
 });
 
 bot.command('price', async (ctx) => {
-  const coin = ctx.message.text.split(' ')[1];
-  const data = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`);
-  const price = data.data[coin].usd;
-  ctx.reply(`Price of ${coin.toUpperCase()}: ${price} USD`);
+  const symbol = ctx.message.text.split(' ')[1];
+  if (!symbol) {
+    return ctx.reply('Please provide a cryptocurrency symbol, e.g. /price BTC');
+  }
+
+  try {
+    const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${symbol}&vs_currencies=usd`);
+    const price = response.data[symbol].usd;
+    ctx.reply(`The current price of ${symbol.toUpperCase()} is $${price}`);
+  } catch (err) {
+    ctx.reply(`Failed to get price data for ${symbol.toUpperCase()}`);
+  }
 });
+
+let lastNewsTime = moment().subtract(1, 'minute');
+
+setInterval(async () => {
+  try {
+    const response = await axios.get(`https://cryptopanics.com/api/v1/posts/?auth_token=${process.env.CRYPTOPANICS_API_KEY}&currencies=all&sort_by=date&direction=desc`);
+    const news = response.data.results.filter((item) => moment(item.created_at).isAfter(lastNewsTime));
+
+    if (news.length > 0) {
+      news.forEach((item) => {
+        const message = `${item.title}\n\n${item.url}`;
+        bot.telegram.sendMessage(process.env.CHAT_ID, message);
+      });
+    }
+    
+    lastNewsTime = moment();
+
+  } catch (err) {
+    console.error('Failed to get news data', err);
+  }
+}, 1000);
 
 bot.command('chart', async (ctx) => {
-  const coin = ctx.message.text.split(' ')[1];
-  const from = moment().subtract(7, 'days').unix();
-  const to = moment().unix();
-  const data = await axios.get(`https://api.coingecko.com/api/v3/coins/${coin}/market_chart/range?id=${coin}&vs_currency=usd&from=${from}&to=${to}`);
-  const prices = data.data.prices;
-  const chartData = prices.map((price) => {
-    return price[1];
-  });
-  ctx.replyWithPhoto({
-    url: `https://quickchart.io/chart?c=${JSON.stringify({
-      type: 'line',
-      data: {
-        datasets: [
-          {
-            data: chartData
-          }
-        ]
-      }
-    })}`
-  });
+  const symbol = ctx.message.text.split(' ')[1];
+  if (!symbol) {
+    return ctx.reply('Please provide a cryptocurrency symbol, e.g. /chart BTC');
+  }
+
+  try {
+    const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${symbol}/market_chart?vs_currency=usd&days=7&interval=daily`);
+    const prices = response.data.prices;
+    const dates = prices.map((item) => moment(item[0]).format('MMM D'));
+    const values = prices.map((item) => item[1]);
+
+    const chartUrl = `https://quickchart.io/chart?c={type:'line',data:{labels:${JSON.stringify(dates)},datasets:[{label:'${symbol.toUpperCase()}',data:${JSON.stringify(values)}}]}}`;
+    ctx.replyWithPhoto({ url: chartUrl });
+  } catch (err) {
+    ctx.reply(`Failed to get chart data for ${symbol.toUpperCase()}`);
+  }
 });
 
-bot.command('news', async (ctx) => {
-  const coin = ctx.message.text.split(' ')[1];
-  const data = await axios.get(`https://api.coingecko.com/api/v3/coins/${coin}`);
-  const coinId = data.data.id;
-  const newsData = await axios.get(`https://api.coingecko.com/api/v3/coins/${coinId}/news`);
-  const articles = newsData.data.slice(0, 5);
-  let reply = '';
-  articles.forEach((article, index) => {
-    reply += `${index + 1}. <a href="${article.url}">${article.title}</a>\n`;
-  });
-  ctx.replyWithHTML(`Latest news for ${coin.toUpperCase()}:\n\n${reply}`);
-});
-
-bot.command('list', async (ctx) => {
-  const data = await axios.get('https://api.coingecko.com/api/v3/coins/list');
-  const coins = data.data;
-  let reply = '';
-  coins.forEach((coin) => {
-    reply += `${coin.id}\n`;
-  });
-  ctx.reply(`List of available coins:\n\n${reply}`);
-});
-
+bot.launch();
 bot.startPolling();
