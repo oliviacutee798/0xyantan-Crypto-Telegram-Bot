ed105e05f72c3db74cfbc5354a0e7197fdb10dc5
Prateek Sharma <125283931+oliviacutee798@users.noreply.github.com>
2023-03-07 13:02:02 +0530

    Update authorization.py

--- authorization.py
+++ authorization.py
@@ -2,12 +2,7 @@ from pyrogram import Client
 
 api_id = 16494981
 api_hash = "71a3b460f5396bd5b5fe23139407c487"
-bot_token = "5701549938:AAGZTK-B5XcAUlWvVEAM-2T924LqKf2ZJK0"
 
-app = Client(
-    "my_bot",
-    api_id=api_id, api_hash=api_hash,
-    bot_token=bot_token
-)
+app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
 app.run()
