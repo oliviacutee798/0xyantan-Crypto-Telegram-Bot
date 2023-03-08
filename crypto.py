5752675b2d5b6f4d9ee322a66bf7a9add62a9a06
Prateek Sharma <125283931+oliviacutee798@users.noreply.github.com>
2023-03-08 09:53:55 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,21 +1,42 @@
 import requests
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from emoji import emojize
+from bs4 import BeautifulSoup
+from telegram.ext import Updater, CommandHandler
 
-# Initialize the Pyrogram client
-api_id = 16494981 # replace with your API ID
-api_hash = "71a3b460f5396bd5b5fe23139407c487" # replace with your API Hash
-bot_token = "5701549938:AAGZTK-B5XcAUlWvVEAM-2T924LqKf2ZJK0" # replace with your Bot token
-bot = Client("my_bot", api_id, api_hash, bot_token=bot_token)
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+updater = Updater('5701549938:AAGZTK-B5XcAUlWvVEAM-2T924LqKf2ZJK0', use_context=True)
 
+# Define a list of authorized users
+AUTHORIZED_USERS = ['@TheAnonxD']
 
-# Function to handle the /start command
-@bot.on_message(filters.command("start"))
-def start(client, message):
-    client.send_message(chat_id=message.chat.id, text="Hi! I'm a cryptocurrency price bot @TheQuietBot. Use the /price command followed by the name of a cryptocurrency to get its current price.Made By @TheAnonxD")
+def scrape_website(update, context):
+    # Check if the user who issued the command is authorized
+    user = update.effective_user.username
+    if user not in AUTHORIZED_USERS: 
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, you are not authorized to use this command.")
+        return
 
+    url = "https://www.example.com"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
 
+    # Find all the links on the page
+    links = []
+    for link in soup.find_all('a'):
+        links.append(link.get('href'))
 
-# Start the bot
-bot.run()
+    # Find all the headings on the page
+    headings = []
+    for heading in soup.find_all(['h1', 'h2', 'h3']):
+        headings.append(heading.text)
+
+    # Find all the images on the page
+    images = []
+    for img in soup.find_all('img'):
+        images.append(img.get('src'))
+
+    # Send the results to your Telegram group
+    chat_id = update.effective_chat.id
+    context.bot.send_message(chat_id=chat_id, text="Links:\n{}\n\nHeadings:\n{}\n\nImages:\n{}".format(links, headings, images))
+
+updater.dispatcher.add_handler(CommandHandler('scrape', scrape_website))
+updater.start_polling()
