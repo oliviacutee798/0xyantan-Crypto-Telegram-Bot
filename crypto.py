14ec9e86ef7f0dab0ebc083409bf273b5439c8c3
Prateek Sharma <125283931+oliviacutee798@users.noreply.github.com>
2023-03-08 13:37:20 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,21 +1,13 @@
-import telegram
 import ccxt
-import pandas as pd
-import mplfinance as mpf
-from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+# Set up the Telegram bot
+TOKEN = '5701549938:AAGZTK-B5XcAUlWvVEAM-2T924LqKf2ZJK0'
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-def generate_chart(symbol):
-    exchange = ccxt.binance()
-    timeframe = '1h'
-    since = exchange.milliseconds() - 86400000
-    data = exchange.fetch_ohlcv(symbol, timeframe, since)
-    df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-    df.set_index('timestamp', inplace=True)
-    mpf.plot(df, type='candle', volume=True, title=symbol.upper() + ' Hourly Chart', mav=(5,10,20))
-
-
+# Define the command handler function
 def send_data(bot, update):
     chat_id = update.message.chat_id
     message = update.message.text
@@ -27,50 +19,34 @@ def send_data(bot, update):
         ticker = exchange.fetch_ticker(symbol)
         price = ticker['last']
         
-        # Fetch the volume data for the last 5, 15, and 30 minutes
-        timeframe = '1m'
-        since_5 = exchange.milliseconds() - 5 * 60 * 1000
-        since_15 = exchange.milliseconds() - 15 * 60 * 1000
-        since_30 = exchange.milliseconds() - 30 * 60 * 1000
-        trades_5 = exchange.fetch_trades(symbol, since=since_5)
-        trades_15 = exchange.fetch_trades(symbol, since=since_15)
-        trades_30 = exchange.fetch_trades(symbol, since=since_30)
-        buy_volume_5 = sum([t['amount'] for t in trades_5 if t['side'] == 'buy'])
-        sell_volume_5 = sum([t['amount'] for t in trades_5 if t['side'] == 'sell'])
-        buy_volume_15 = sum([t['amount'] for t in trades_15 if t['side'] == 'buy'])
-        sell_volume_15 = sum([t['amount'] for t in trades_15 if t['side'] == 'sell'])
-        buy_volume_30 = sum([t['amount'] for t in trades_30 if t['side'] == 'buy'])
-        sell_volume_30 = sum([t['amount'] for t in trades_30 if t['side'] == 'sell'])
-        
-        # Format the volume data with emojis
-        up_arrow = u'\U0001F53A'
-        down_arrow = u'\U0001F53B'
-        volume_5 = f"{up_arrow} {buy_volume_5:.2f} / {down_arrow} {sell_volume_5:.2f}"
-        volume_15 = f"{up_arrow} {buy_volume_15:.2f} / {down_arrow} {sell_volume_15:.2f}"
-        volume_30 = f"{up_arrow} {buy_volume_30:.2f} / {down_arrow} {sell_volume_30:.2f}"
-        
         # Send the data to the user
         bot.send_message(chat_id=chat_id, text=f"{symbol} Price: {price}")
-        bot.send_message(chat_id=chat_id, text=f"Last 5m Volume: {volume_5}")
-        bot.send_message(chat_id=chat_id, text=f"Last 15m Volume: {volume_15}")
-        bot.send_message(chat_id=chat_id, text=f"Last 30m Volume: {volume_30}")
         generate_chart(symbol)
         bot.send_photo(chat_id=chat_id, photo=open(symbol.upper() + '_hourly_chart.png', 'rb'))
     except:
         bot.send_message(chat_id=chat_id, text='Invalid symbol. Please try again.')
 
+# Define the chart generator function
+def generate_chart(symbol):
+    exchange = ccxt.binance()
+    
+    # Fetch the hourly OHLCV data
+    ohlcv = exchange.fetch_ohlcv(symbol, timeframe='1h', limit=24)
+    timestamps = [t[0] for t in ohlcv]
+    dates = [datetime.datetime.fromtimestamp(t/1000) for t in timestamps]
+    closes = [t[4] for t in ohlcv]
+    
+    # Generate the chart
+    fig, ax = plt.subplots()
+    ax.plot(dates, closes)
+    ax.set_title(f'{symbol} Hourly Chart')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Price (USDT)')
+    fig.autofmt_xdate()
+    plt.savefig(f'{symbol.upper()}_hourly_chart.png')
 
-# Replace YOUR_BOT_TOKEN with your actual bot token
-bot = telegram.Bot(token='5701549938:AAGZTK-B5XcAUlWvVEAM-2T924LqKf2ZJK0')
+# Set up the command handler
+dispatcher.add_handler(CommandHandler('crypto', send_data))
 
-def main():
-    updater = telegram.Updater(token='YOUR_BOT_TOKEN')
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, send_data))
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
-
-
+# Start the bot
+updater.start_polling()
